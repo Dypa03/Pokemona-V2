57fa1e73de2e0333edfd1e9cd934aff59447660b
Dypa03 <davidedipa03@gmail.com>
2025-01-15 03:15:15 +0100

    FUCKING FIXED THAT BUG, JAVA FU

--- Backend/teambuilder-service/teambuilder-service/src/main/java/com/teambuilder_service/TeambuilderServiceApplication.java
+++ Backend/teambuilder-service/teambuilder-service/src/main/java/com/teambuilder_service/TeambuilderServiceApplication.java
@@ -1,6 +1,5 @@
 package com.teambuilder_service;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
